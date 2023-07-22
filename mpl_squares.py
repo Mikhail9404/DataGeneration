314eacb53a23d511fafe96ac147f179545a2dfd5
Mikhail Kuznetsov <misha9404@yandex.ru>
2023-07-22 23:14:43 +0300

    Generic date, start.

--- mpl_squares.py
+++ mpl_squares.py
@@ -7,12 +7,12 @@ plt.style.use('seaborn-v0_8-darkgrid')
 fig, ax = plt.subplots()
 ax.plot(input_values, squares, linewidth = 3)
 
-# Назначение заголовка диаграммы и меток осей.
+# Назначение заголовка диаграммы и меток осей
 ax.set_title("Square Numbers", fontsize = 24)
 ax.set_xlabel("Value", fontsize = 14)
 ax.set_ylabel("Square of Value", fontsize = 14)
 
-# Назначение размера шрифта делений на осях.
+# Назначение размера шрифта делений на осях
 ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
--- scatter_squares.py
+++ scatter_squares.py
@@ -7,15 +7,15 @@ plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
-# Назначение заголовка диаграммы и меток осей.
+# Назначение заголовка диаграммы и меток осей
 ax.set_title("Square Numbers", fontsize = 24)
 ax.set_xlabel("Value", fontsize = 14)
 ax.set_ylabel("Square of Value", fontsize = 14)
 
-# Назначение размера шрифта делений на осях.
+# Назначение размера шрифта делений на осях
 ax.tick_params(axis='both', which='major', labelsize=14)
 
-# Назначение диапазона для каждой оси.
+# Назначение диапазона для каждой оси
 ax.axis([0, 1100, 0, 1_100_000])
 
 #plt.show()
