6881f5b7e87bceaf0a7474b86734c79fbe51424a
Mikhail <Misha9404@yandex.ru>
2023-07-24 16:05:18 +0300

    RandomWalker

---
+++ RandomWalker/rw_visual.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+from random_walk import RandomWalk
+
+# Построение случайного блуждения.
+rw = RandomWalk()
+rw.fill_walk()
+
+# Нанесение точек на диаграмму.
+plt.style.use('classic')
+fig, ax = plt.subplots()
+ax.scatter(rw.x_values, rw.y_values, s=15)
+
+plt.show()
