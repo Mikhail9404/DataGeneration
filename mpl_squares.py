1dfa1ac30fbc6dc9acfc7a9a16a693c01c606cdb
Mikhail Kuznetsov <misha9404@yandex.ru>
2023-08-02 20:00:16 +0300

    Generic date, start.

--- mpl_squares.py
+++ mpl_squares.py
@@ -8,7 +8,7 @@ fig, ax = plt.subplots()
 ax.plot(input_values, squares, linewidth = 3)
 
 # Назначение заголовка диаграммы и меток осей
-ax.set_title("Square Numbers", fontsize = 24)
+ax.set_title("Square Numbers", fontsize = 25)
 ax.set_xlabel("Value", fontsize = 14)
 ax.set_ylabel("Square of Value", fontsize = 14)
 
--- scatter_squares.py
+++ scatter_squares.py
@@ -8,7 +8,7 @@ fig, ax = plt.subplots()
 ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Назначение заголовка диаграммы и меток осей
-ax.set_title("Square Numbers", fontsize = 24)
+ax.set_title("Square Numbers", fontsize = 25)
 ax.set_xlabel("Value", fontsize = 14)
 ax.set_ylabel("Square of Value", fontsize = 14)
 
